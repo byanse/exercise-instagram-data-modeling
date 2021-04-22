10f1ba6e95aff039cefeaf9602e33f0a06f8021d
byanse <ulloaby@gmail.com>
2021-04-22 19:09:43 +0000

    modelado de tablas ig

--- src/models.py
+++ src/models.py
@@ -8,23 +8,49 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    birthdate= Column(String(250), nullable=False)
+    img_profile = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Posts(Base):
+    __tablename__ = 'posts'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_type = Column(String(250))
+    post_place = Column(String(250))
+    post_comment = Column(String(250))
+    created_at = Column(String(250))
+    updated_at = Column(String(250))
+
+class Post_comments(Base):
+    __tablename__ = 'post_comments'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    comment = Column(String(250))
+    created_at = Column(String(250))
+    delete_at = Column(String(250))
+
+class Post_likes(Base):
+    __tablename__ = 'post_likes'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    created_at = Column(String(250))
+    delete_at = Column(String(250))
 
     def to_dict(self):
         return {}
